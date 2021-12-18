b2bb8cda2d8ba0108d896674266ed95b82a91c76
NonSinat <sinat.non@student.niptict.edu.kh>
2021-12-18 10:38:09 +0700

    closed 16 Store User Data

--- lib/screen/authenication_screen/registration_screen.dart
+++ lib/screen/authenication_screen/registration_screen.dart
@@ -51,231 +51,226 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               child: Form(
                 key: formKey,
                 child: Column(
-                      // ignore: prefer_const_literals_to_create_immutables
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: [
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * .04,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .04,
+                    ),
+                    Text(
+                      "សូមស្វាគមន៍មកកាន់ទីនេះ",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: "Poppins",
+                        color: Color(0xff979797),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "បំពេញទម្រង់បែបបទដើម្បីក្លាយជាភ្ញៀវរបស់យើង",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: "Poppins",
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .1,
+                    ),
+                    Text(
+                      "សម្រាប់ឧទាហរណ៍​: ជ្រើសរើស កូដប្រទេស​​ លេខទូរសព្ទ័",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: "Poppins",
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * .05),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: Colors.black.withOpacity(.25),
                         ),
-                        Text(
-                          "សូមស្វាគមន៍មកកាន់ទីនេះ",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontFamily: "Poppins",
-                            color: Color(0xff979797),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(.25),
+                            offset: Offset(1.0, 1.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 1.0,
                           ),
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          "បំពេញទម្រង់បែបបទដើម្បីក្លាយជាភ្ញៀវរបស់យើង",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: "Poppins",
+                          BoxShadow(
+                            color: Colors.black.withOpacity(.25),
+                            offset: Offset(-1.0, -1.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 1.0,
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 4.0),
+                        child: InternationalPhoneNumberInput(
+                          textFieldController: controller,
+                          onInputChanged: (PhoneNumber number) {
+                            setState(() {
+                              dialCode = number.dialCode;
+                            });
+                          },
+                          onInputValidated: (bool value) {
+                            print(value);
+                          },
+                          selectorConfig: SelectorConfig(
+                            selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+                          ),
+                          ignoreBlank: false,
+                          autoValidateMode: AutovalidateMode.disabled,
+                          selectorTextStyle: TextStyle(
                             color: Colors.black,
-                            fontWeight: FontWeight.w500,
+                            fontFamily: "Poppins",
                           ),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * .1,
-                        ),
-                        Text(
-                          "សម្រាប់ឧទាហរណ៍​: ជ្រើសរើស កូដប្រទេស​​ លេខទូរសព្ទ័",
-                          style: TextStyle(
+                          initialValue: number,
+                          maxLength: 9,
+                          hintText: "ឧ​.ទា 123 456 7",
+                          keyboardType: TextInputType.numberWithOptions(
+                            signed: true,
+                            decimal: true,
+                          ),
+                          inputBorder: InputBorder.none,
+                          textStyle: TextStyle(
+                            fontFamily: "Poppins",
                             fontSize: 16,
-                            fontFamily: "Poppins",
+                          ),
+                          onSaved: (PhoneNumber number) {
+                            print('On Saved: $number');
+                            setState(
+                              () {
+                                dialCode == number;
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .05,
+                    ),
+                    Center(
+                      child: Column(
+                        children: [
+                          RaisedButton(
+                            splashColor: Colors.white.withOpacity(.25),
                             color: Colors.black,
-                          ),
-                        ),
-                        SizedBox(
-                            height: MediaQuery.of(context).size.height * .05),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                              color: Colors.black.withOpacity(.25),
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(.25),
-                                offset: Offset(1.0, 1.0),
-                                blurRadius: 10.0,
-                                spreadRadius: 1.0,
+                            shape: CircleBorder(),
+                            onPressed: () async {
+                              _authentication.signInwithPhoneNumber(
+                                  my_phone_num: "$dialCode${controller.text}");
+                              print("$dialCode${controller.text}");
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(14.0),
+                              child: Icon(
+                                Icons.chevron_right,
+                                size: 30,
+                                color: Colors.white,
                               ),
-                              BoxShadow(
-                                color: Colors.black.withOpacity(.25),
-                                offset: Offset(-1.0, -1.0),
-                                blurRadius: 10.0,
-                                spreadRadius: 1.0,
-                              ),
-                            ],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10.0, vertical: 4.0),
-                            child: InternationalPhoneNumberInput(
-                              textFieldController: controller,
-                              onInputChanged: (PhoneNumber number) {
-                                setState(() {
-                                  dialCode = number.dialCode;
-                                });
-                              },
-                              onInputValidated: (bool value) {
-                                print(value);
-                              },
-                              selectorConfig: SelectorConfig(
-                                selectorType:
-                                    PhoneInputSelectorType.BOTTOM_SHEET,
-                              ),
-                              ignoreBlank: false,
-                              autoValidateMode: AutovalidateMode.disabled,
-                              selectorTextStyle: TextStyle(
-                                color: Colors.black,
-                                fontFamily: "Poppins",
-                              ),
-                              initialValue: number,
-                              maxLength: 9,
-                              hintText: "ឧ​.ទា 123 456 7",
-                              keyboardType: TextInputType.numberWithOptions(
-                                signed: true,
-                                decimal: true,
-                              ),
-                              inputBorder: InputBorder.none,
-                              textStyle: TextStyle(
-                                fontFamily: "Poppins",
-                                fontSize: 16,
-                              ),
-                              onSaved: (PhoneNumber number) {
-                                print('On Saved: $number');
-                                setState(
-                                  () {
-                                    dialCode == number;
-                                  },
-                                );
-                              },
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * .05,
-                        ),
-                        Center(
+                          Text(
+                            "បន្ត",
+                            style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * .02,
+                          ),
+                          Text(
+                            "ឬ",
+                            style:
+                                TextStyle(fontFamily: "Poppins", fontSize: 14),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * .02,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        InkWell(
+                          onTap: () async {
+                            _authentication.signInWithGoogle();
+
+                            print("Google");
+                          },
                           child: Column(
                             children: [
-                              RaisedButton(
-                                splashColor: Colors.white.withOpacity(.25),
-                                color: Colors.black,
-                                shape: CircleBorder(),
-                                onPressed: () async {
-                                  _authentication.signInwithPhoneNumber(
-                                      my_phone_num:
-                                          "$dialCode${controller.text}");
-                                  print("$dialCode${controller.text}");
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.all(14.0),
-                                  child: Icon(
-                                    Icons.chevron_right,
-                                    size: 30,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
+                              SvgPicture.asset("assets/icons/google.svg"),
                               Text(
-                                "បន្ត",
-                                style: TextStyle(
-                                  fontFamily: "Poppins",
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * .02,
-                              ),
-                              Text(
-                                "ឬ",
+                                "Google",
                                 style: TextStyle(
                                     fontFamily: "Poppins", fontSize: 14),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * .02,
                               ),
                             ],
                           ),
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            InkWell(
-                              onTap: () async {
-                                _authentication.signInWithGoogle();
-
-                                print("Google");
-                              },
-                              child: Column(
-                                children: [
-                                  SvgPicture.asset("assets/icons/google.svg"),
-                                  Text(
-                                    "Google",
-                                    style: TextStyle(
-                                        fontFamily: "Poppins", fontSize: 14),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            InkWell(
-                              onTap: () {
-                                print("Facebook");
-                              },
-                              child: Column(
-                                children: [
-                                  SvgPicture.asset("assets/icons/google.svg"),
-                                  Text(
-                                    "Facebook",
-                                    style: TextStyle(
-                                      fontFamily: "Poppins",
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        Container(
-                          alignment: Alignment.bottomCenter,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                        InkWell(
+                          onTap: () {
+                            print("Facebook");
+                          },
+                          child: Column(
                             children: [
+                              Image.asset("assets/icons/Facebook.png"),
                               Text(
-                                "តើអ្នកមានគណនីហើយឬនៅ?",
+                                "Facebook",
                                 style: TextStyle(
-                                    fontFamily: "Poppins", fontSize: 14),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  print("Login");
-                                },
-                                child: Text(
-                                  "ចូលប្រើ នៅទីនេះ",
-                                  style: TextStyle(
-                                    fontFamily: "Poppins",
-                                    fontSize: 14,
-                                    color: ThemeConstant.lightScheme.primary,
-                                  ),
+                                  fontFamily: "Poppins",
+                                  fontSize: 14,
                                 ),
                               ),
                             ],
                           ),
                         ),
                       ],
+                    ),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "តើអ្នកមានគណនីហើយឬនៅ?",
+                            style:
+                                TextStyle(fontFamily: "Poppins", fontSize: 14),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              print("Login");
+                            },
+                            child: Text(
+                              "ចូលប្រើ នៅទីនេះ",
+                              style: TextStyle(
+                                fontFamily: "Poppins",
+                                fontSize: 14,
+                                color: ThemeConstant.lightScheme.primary,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
