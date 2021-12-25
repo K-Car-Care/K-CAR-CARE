6568974079bfb84ec4fa9e729edacfa1e0daae98
NonSinat <sinat.non@student.niptict.edu.kh>
2021-12-25 14:39:22 +0700

    closed #25 Notification Onesignal

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_print
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:k_car_care_project/constant/theme_constant.dart';
 import 'package:k_car_care_project/screen/authenication_screen/registration_screen.dart';
+import 'package:k_car_care_project/screen/home_screen/home_screen.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,11 +23,51 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: ThemeConstant.lightScheme.background,
-        appBarTheme: AppBarTheme(color: ThemeConstant.lightScheme.background,),
+        appBarTheme: AppBarTheme(
+          color: ThemeConstant.lightScheme.background,
+        ),
       ),
       debugShowCheckedModeBanner: false,
       //Hello
-      home:  RegistrationScreen(),
+      home: ScreenWrapper(),
     );
+  }
+}
+
+class ScreenWrapper extends StatefulWidget {
+  const ScreenWrapper({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenWrapper> createState() => _ScreenWrapperState();
+}
+
+class _ScreenWrapperState extends State<ScreenWrapper> {
+  static const String onesignalAppId = "3b2417ee-ec74-4dac-9d04-b83a9c71cf2d";
+
+  @override
+  void initState() {
+    initPlatform();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RegistrationScreen();
+  }
+
+  Future<void> initPlatform() async {
+    OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+    OneSignal.shared.setAppId(onesignalAppId);
+    OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+      // print("Accepted permission: $accepted");
+    });
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {});
+    OneSignal.shared.setNotificationOpenedHandler((openedResult) {
+      var data = openedResult.notification.title;
+      print(data);
+      Get.to(() => MyHomeScreen());
+    });
   }
 }
