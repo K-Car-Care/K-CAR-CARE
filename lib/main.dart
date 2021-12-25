7734c5b18158cf26dea98ce0e4d6e12612fd9964
NonSinat <sinat.non@student.niptict.edu.kh>
2021-12-25 12:55:57 +0700

    closed #23 Alert Magazine Notificaiton

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:k_car_care_project/constant/theme_constant.dart';
-import 'package:k_car_care_project/screen/authenication_screen/registration_screen.dart';
+import 'package:k_car_care_project/model/magazine_notification_model.dart';
+import 'package:k_car_care_project/screen/notification_screen/notification_badges.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,14 +21,174 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: ThemeConstant.lightScheme.background,
-        appBarTheme: AppBarTheme(color: ThemeConstant.lightScheme.background,),
+    return OverlaySupport(
+      child: GetMaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: ThemeConstant.lightScheme.background,
+          appBarTheme: AppBarTheme(
+            color: ThemeConstant.lightScheme.background,
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        //Hello
+        home: HomePage(),
       ),
-      debugShowCheckedModeBanner: false,
-      //Hello
-      home:  RegistrationScreen(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final FirebaseMessaging _message;
+  late int _totalNotificationCounter;
+  //model
+
+  PushMagazineNotificationModel? _notificationInfo;
+
+  // Register Notification
+  void registerNotification() async {
+    _message = FirebaseMessaging.instance;
+
+    //three type of state in notificaiton
+    //not determined (null), granted (true) and decline (false)
+
+    NotificationSettings settings = await _message.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print("User granted the permission");
+
+      //main message
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        PushMagazineNotificationModel notification =
+            PushMagazineNotificationModel(
+          title: message.notification?.title,
+          body: message.notification?.body,
+          dataTitle: message.data['title'],
+          dataBody: message.data['body'],
+        );
+        setState(() {
+          _totalNotificationCounter++;
+          _notificationInfo = notification;
+        });
+        if (notification != null) {
+          showSimpleNotification(
+            Text(
+              _notificationInfo!.title.toString(),
+            ),
+            leading: NotificationBadges(
+              totalNotification: _totalNotificationCounter,
+            ),
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.cyan.shade700,
+            duration: Duration(seconds: 2),
+          );
+        }
+      });
+    } else {
+      print("permission declined by user");
+    }
+  }
+
+  @override
+  void initState() {
+    // Message In background App
+    FirebaseMessaging.onMessageOpenedApp.listen(
+      (RemoteMessage message) {
+        PushMagazineNotificationModel notification =
+            PushMagazineNotificationModel(
+          title: message.notification?.title,
+          body: message.notification?.body,
+          dataTitle: message.data['title'],
+          dataBody: message.data['hello'],
+        );
+        setState(() {
+          _totalNotificationCounter++;
+          _notificationInfo = notification;
+        });
+      },
+    );
+
+    checkForInitialMessage();
+    super.initState();
+    registerNotification();
+    _totalNotificationCounter = 0;
+  }
+
+  // Check the initial message that we receive
+  checkForInitialMessage() async {
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+    if (initialMessage != null) {
+      PushMagazineNotificationModel notification =
+          PushMagazineNotificationModel(
+        title: initialMessage.notification?.title,
+        body: initialMessage.notification?.body,
+        dataTitle: initialMessage.data['title'],
+        dataBody: initialMessage.data['body'],
+      );
+      setState(() {
+        _totalNotificationCounter++;
+        _notificationInfo = notification;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Push Notification Screen"),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Flutter PushNotification",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 20,
+            ),
+          ),
+          NotificationBadges(
+            totalNotification: _totalNotificationCounter,
+          ),
+          //if notificationInfo is not null
+          _notificationInfo != null
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Title: ${_notificationInfo!.dataTitle ?? _notificationInfo!.title}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      "Title: ${_notificationInfo!.dataBody ?? _notificationInfo!.body}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                )
+              : Container(),
+        ],
+      )),
     );
   }
 }
---
+++ lib/screen/notification_screen/notification_badges.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class NotificationBadges extends StatelessWidget {
+  final int totalNotification;
+  const NotificationBadges({
+    Key? key,
+    required this.totalNotification,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Text(
+            "$totalNotification",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
